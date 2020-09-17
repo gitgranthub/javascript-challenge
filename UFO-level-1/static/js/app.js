ba57dcdf0bfea7ae792ef327e605254de344f8f2
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-09-17 13:00:51 -0500

    filter working but attempting to get 'return' button working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,73 +24,123 @@ data.forEach(function(ufoReports) {
   });
 });
 
-
-
-
-
-// Delete rows function
-function deleteRows() {
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  // Clear table
-  d3.selectAll(".tb").remove();
-}
-
-
-// Complete the event handler function for the form -n
-function runEnter() {
-  
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
-  // Log/Test filter result
-  console.log(`The date choosen is ${inputValue}`);
-
-
-  // Get a reference to the table body -r
-  var tbody = d3.select(".tb");
-
-  var fData = data.filter(data => data.datetime === inputValue); // -t
-  // Get a reference to the table body
-  var filteredData = fData;
-
-  console.log(`The filtered data is: ${filteredData}`);
-  console.log(data);
-
-
-  filteredData.forEach(function(filterReports) {
-    console.log(filterReports);
-    var row = tbody.append("tr");
-    Object.entries(filterReports).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      // in the ufo report object
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-};
-
 // Create event handlers 
 // Select the button
 var button = d3.select("#filter-btn");
 
+button.on("click", function() {
+
+
+  // Clear table before populating with Filtered Data
+  tbody.html("");
+
+  // Select the input date get the raw HTML nodes
+  var inputElement = d3.select("#datetime");
+  // Get the value property of the input date, state, shape
+  var inputValue = inputElement.property("value");
+  // console.log input value
+  console.log(inputValue);
+  // Filter Data with datetime equal to input value
+  var filteredData = tableData.filter(data => data.datetime === inputValue);
+  // console.log filter values
+  console.log(filteredData);
+
+
+  filteredData.forEach(function(filterReports) {
+
+  console.log(filterReports);
+  // Append one table row `tr` for each UFO Sighting object
+  var row = tbody.append("tr");
+  // Use `Object.entries` to console.log each UFO Sighting value
+  Object.entries(filterReports).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+  });
+});
+});
+
 // Select the form
+
 var form = d3.select("#form");
+var returnForm = from.selectAll(null)
+.on("keypress", function() {
+  if(d3.event.keyCode === 13){
+  console.log("you pressed enter!")
+  // Clear table before populating with Filtered Data
+  tbody.html("");
 
-button.on("click", function(d) {
-  deleteRows(d);
-  runEnter(d);
+  // Select the input date get the raw HTML nodes
+  var inputElement = d3.select("#datetime");
+  // Get the value property of the input date, state, shape
+  var inputValue = inputElement.property("value");
+  // console.log input value
+  console.log(inputValue);
+  // Filter Data with datetime equal to input value
+  var filteredData = tableData.filter(data => data.datetime === inputValue);
+  // console.log filter values
+  console.log(filteredData);
+
+
+  filteredData.forEach(function(filterReports) {
+
+    console.log(filterReports);
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(filterReports).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    }
+  }
+  })
 });
 
-form.on("submit", function(d) {
-  deleteRows(d);
-  runEnter(d);
-});
+  
+  
+
+
+
+// // Complete the event handler function for the form -n
+// function runEnter() {
+  
+
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
+  
+//   // Select the input element and get the raw HTML node
+//   var inputElement = d3.select("#datetime");
+
+//   // Get the value property of the input element
+//   var inputValue = inputElement.property("value");
+
+//   // Log/Test filter result
+//   console.log(`The date choosen is ${inputValue}`);
+
+
+//   // Get a reference to the table body -r
+//   var tbody = d3.select(".tb");
+
+//   var fData = data.filter(data => data.datetime === inputValue); // -t
+//   // Get a reference to the table body
+//   var filteredData = fData;
+
+//   console.log(`The filtered data is: ${filteredData}`);
+//   console.log(data);
+
+
+//   filteredData.forEach(function(filterReports) {
+//     console.log(filterReports);
+//     var row = tbody.append("tr");
+//     Object.entries(filterReports).forEach(function([key, value]) {
+//       console.log(key, value);
+//       // Append a cell to the row for each value
+//       // in the ufo report object
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
+// }
