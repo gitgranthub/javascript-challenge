bec38ed0ece5c4976c2f13037e10cc8171e1d678
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-09-17 09:38:58 -0500

    stuck on removing rows before filter populating

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,20 +10,18 @@ data.forEach(function(ufoReports) {
     console.log(ufoReports);
   });
 
-
-
 // // Use d3 to update each cell's text with
 // // ufo report values (Date, City, State, Country, Shape, Duration, Comments) -a
 data.forEach(function(ufoReports) {
-    console.log(ufoReports);
-    var row = tbody.append("tr");
-    Object.entries(ufoReports).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      // in the ufo report object
-      var cell = row.append("td");
-      cell.text(value);
-  });
+  console.log(ufoReports);
+  var row = tbody.append("tr");
+  Object.entries(ufoReports).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the ufo report object
+    var cell = row.append("td");
+    cell.text(value);
+});
 });
 
 // Select the button
@@ -39,27 +37,36 @@ form.on("submit",runEnter);
 
 // Complete the event handler function for the form -n
 function runEnter() {
+  
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
-  tbody.html("");
-  rows.exit().remove();
-  cells.exit().remove();
+  
+
+  // Clear table
+  var table = d3.select("#ufo-table");
+  //var tbody = d3.select("#ufo-table");
+  //or use :  var table = document.all.tableid;
+  function DeleteRows() {
+    var rowCount = ufo-table.rows.length;
+    for (var i = rowCount - 1; i > 0; i--) {
+      ufo-table.deleteRow(i);
+    }
+  }
+
+  
+  
+  
+  //tbody.html("");
+  
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
+  // Log/Test filter result
   console.log(`The date choosen is ${inputValue}`);
-//   console.log(tableData);
-//   console.log(tableData.datetime);
-//   console.log(tableData.city);
-//   console.log(tableData.state);
-//   console.log(tableData.country);
-//   console.log(tableData.shape);
-//   console.log(tableData.durationMinutes);
-//   console.log(tableData.comments);
 
   var filteredData = data.filter(data => data.datetime === inputValue); // -t
   // Get a reference to the table body
