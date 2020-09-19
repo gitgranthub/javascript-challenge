04c4d097c243a4b396198a195ecd2546061530c1
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-09-18 22:17:49 -0500

    versioning through key event listener code attempts

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -85,41 +85,80 @@ button2.on("click", function() {
 
 
 // KEYPRESS INSTEAD OF CLICK BUTTON SUMBIT CODE
+function keydown(event) {
+  if (event.defaultPrevented) {
+      return;
+  }
 
-function btnSubmitCheck(event) {
-  
-  // TRYING TO STOP 404 ERROR on keypress ****
-  //res.sendFile('../index.html');
-  // Clear table before populating with Filtered Data
-  tbody.html("");
-  // Prevent the page from refreshing
-  //d3.event.preventDefault();
-  // Select the input date get the raw HTML nodes
-  var inputElement = d3.select("#datetime");
-  // Get the value property of the input date, state, shape
-  var inputValue = inputElement.property("value");
-  // console.log input value
-  console.log(inputValue);
-  // Filter Data with datetime equal to input value
-  var filteredData = tableData.filter(data => data.datetime === inputValue);
-  // console.log filter values
-  console.log(filteredData);
+  var key = event.key || event.keyCode;
+
+  if (key === 'Tab' || key === 13) {
+    // Clear table before populating with Filtered Data
+    tbody.html("");
+    // Prevent the page from refreshing
+    //d3.event.preventDefault();
+    // Select the input date get the raw HTML nodes
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    // console.log input value
+    console.log(inputValue);
+    // Filter Data with datetime equal to input value
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+    // console.log filter values
+    console.log(filteredData);
 
 
-  filteredData.forEach(function(filterReports) {
+    filteredData.forEach(function(filterReports) {
 
-  console.log(filterReports);
-  // Append one table row `tr` for each UFO Sighting object
-  var row = tbody.append("tr");
-  // Use `Object.entries` to console.log each UFO Sighting value
-  Object.entries(filterReports).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      var cell = row.append("td");
-      cell.text(value);
+    console.log(filterReports);
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(filterReports).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+      });
     });
-  });
-}
+  }
+};
+
+// function btnSubmitCheck(event) {
+  
+//   // TRYING TO STOP 404 ERROR on keypress ****
+//   //res.sendFile('../index.html');
+//   // Clear table before populating with Filtered Data
+//   tbody.html("");
+//   // Prevent the page from refreshing
+//   //d3.event.preventDefault();
+//   // Select the input date get the raw HTML nodes
+//   var inputElement = d3.select("#datetime");
+//   // Get the value property of the input date, state, shape
+//   var inputValue = inputElement.property("value");
+//   // console.log input value
+//   console.log(inputValue);
+//   // Filter Data with datetime equal to input value
+//   var filteredData = tableData.filter(data => data.datetime === inputValue);
+//   // console.log filter values
+//   console.log(filteredData);
+
+
+//   filteredData.forEach(function(filterReports) {
+
+//   console.log(filterReports);
+//   // Append one table row `tr` for each UFO Sighting object
+//   var row = tbody.append("tr");
+//   // Use `Object.entries` to console.log each UFO Sighting value
+//   Object.entries(filterReports).forEach(function([key, value]) {
+//       console.log(key, value);
+//       // Append a cell to the row for each value
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
+// }
 
 // document.addEventListener('keypress', function (e) {
 //   if (e.key === 'Enter') {
